8f1d47ea00a5196a1d0c402f7bd94dd23783c6ad
OlStar3 <jkmuf3312@yandex.ru>
2025-03-07 14:28:39 +0300

    Test1

--- src/app/pages/auth/authorization/authorization.component.ts
+++ src/app/pages/auth/authorization/authorization.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {NgClass} from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../services/user.service';
 @Component({
   selector: 'app-authorization',
-  imports: [],
+  imports: [NgClass, FormsModule, ButtonModule, InputTextModule],
   templateUrl: './authorization.component.html',
   styleUrl: './authorization.component.scss'
 })
-export class AuthorizationComponent {
+export class AuthorizationComponent implements OnInit {
+  login: string = null;
+  password: string;
+
+constructor(private userService:UserService){
 
 }
+ngOnInit(): void {
+
+}
+onCheck(ev:Event): void {
+console.log('ev', ev);
+
+
+}
+}
+
+
+
+
--- src/app/pages/auth/registration/registration.component.ts
+++ src/app/pages/auth/registration/registration.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CheckboxModule } from 'primeng/checkbox';
+import {NgClass} from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../services/user.service';
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [NgClass, FormsModule,ButtonModule, CheckboxModule, InputTextModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit{
+login: string = null;
+password: string;
+repeatPassword: string;
+cardNumber: string;
+email: string;
+isRemember: boolean;
+labelText = "Сохранить пользователя в хранилище";
+constructor(private userService:UserService){
 
+}
+ngOnInit(): void {
+
+}
+onAuth(ev:Event): void {
+console.log('ev', ev);
+this.userService.addUser({login: this.login, password: this.password});
+
+}
 }
